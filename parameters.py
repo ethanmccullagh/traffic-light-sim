5d07f4e52c755465d0ea681328a7c5fd66e779b5
Ethan McCullagh <84647308+ethan89762@users.noreply.github.com>
2024-12-20 12:45:52 -0800

    Graphs

--- parameters.py
+++ parameters.py
@@ -1,10 +1,11 @@
 params = {
-    'PRINT_EVENTS' : True,
+    'PRINT_EVENTS' : False,
     'NUM_CARS' : 550,
     'ARR_PER_HOUR' : 550,
     'SERVICE_TIME' : 1/8,
     'NUM_RUNS' : 1,
     'lightInterval' : [0.6, 0.4],
     'directionWeights' : [0.3, 0.2, 0.3, 0.2],
-    'instantSwitching' : True
+    'instantSwitching' : True,
+    'randomSeed': 10
 }
