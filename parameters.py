fc0fcbff311e31edaf15a1269afbbb0a93201181
Ethan McCullagh <84647308+ethan89762@users.noreply.github.com>
2024-12-20 19:24:44 -0800

    daoifjodsa

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@ params = {
     'PRINT_EVENTS' : False,
     'NUM_CARS' : 550,
     'ARR_PER_HOUR' : 550,
-    'SERVICE_TIME' : 1/8,
+    'SERVICE_TIME' : 1/4,
     'NUM_RUNS' : 1,
     'lightInterval' : [0.6, 0.4],
     'directionWeights' : [0.3, 0.2, 0.3, 0.2],
